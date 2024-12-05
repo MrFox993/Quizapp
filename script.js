29f43c3e5b2013691a52faff1b70e810783deea8
Marcel Fuchs <marcel.fuchs.1@gmx.de>
2024-12-05 11:16:18 +0100

    visualization of correct/wrong answer && enabling next question button

--- script.js
+++ script.js
@@ -27,6 +27,36 @@ function startQuiz(index) {
 
 function renderQuizContent(index, topic) {
   let quizContentRef = document.getElementById("quiz-content");
+  let questionIndex = 0;
   quizContentRef.innerHTML = "";
-  quizContentRef.innerHTML = getQuizContentHTMLTemplate(index, topic);
+  quizContentRef.innerHTML = getQuizContentHTMLTemplate(index, questionIndex, topic);
+}
+
+function nextQuestion(index, questionIndex, topic) {
+  console.table(index, questionIndex, topic);
+  let newQuestionIndex = questionIndex + 1;
+  let quizContentRef = document.getElementById("quiz-content");
+  quizContentRef.innerHTML = "";
+  quizContentRef.innerHTML = getQuizContentHTMLTemplate(index, newQuestionIndex, topic);
+}
+
+function previousQuestion(index, questionIndex, topic) {
+  let newQuestionIndex = questionIndex - 1;
+  let quizContentRef = document.getElementById("quiz-content");
+  quizContentRef.innerHTML = "";
+  quizContentRef.innerHTML = getQuizContentHTMLTemplate(index, newQuestionIndex, topic);
+}
+
+function selectAnswer(index, questionIndex, answer_index) {
+  let correctAnswer = questions[index][questionIndex].correctAnswer;
+  let chosenAnswerRef = document.getElementById(`quiz-content-answer-${answer_index}`);
+  let correctAnswerRef = document.getElementById(`quiz-content-answer-${correctAnswer}`);
+  let nextQuestionButtonRef = document.getElementById('next-question-button')
+  if (correctAnswer === answer_index) {
+    chosenAnswerRef.classList.add("bg-success");
+  } else {
+    chosenAnswerRef.classList.add("bg-danger");
+    correctAnswerRef.classList.add("bg-success")
+  }
+  nextQuestionButtonRef.disabled = false
 }
--- scripts/templates.js
+++ scripts/templates.js
@@ -1,30 +1,29 @@
-function getQuizContentHTMLTemplate(index, topic) {
-    let defaultQuestionIndex = 0;
-    let currentQuestionIndex = defaultQuestionIndex + 1
+function getQuizContentHTMLTemplate(index, questionIndex, topic) {
+    let currentQuestionNumber = questionIndex + 1
     return `
             <div>
             <h4 id="quiz-content-header">${topic}</h4>
-            <h5 id="quiz-content-question">${questions[index][defaultQuestionIndex].question}</h5>
+            <h5 id="quiz-content-question">${questions[index][questionIndex].question}</h5>
             <div id="quiz-content-answers">
             <ol class="list-group list-group-numbered">
-                <li class="list-group-item" id="quiz-content-answer-1">${questions[index][defaultQuestionIndex].answer_1}</li>
-                <li class="list-group-item" id="quiz-content-answer-2">${questions[index][defaultQuestionIndex].answer_2}</li>
-                <li class="list-group-item" id="quiz-content-answer-3">${questions[index][defaultQuestionIndex].answer_3}</li>
-                <li class="list-group-item" id="quiz-content-answer-4">${questions[index][defaultQuestionIndex].answer_4}</li>
+                <li class="list-group-item quiz-answer-card" id="quiz-content-answer-1" onclick="selectAnswer(${index},${questionIndex},1)">${questions[index][questionIndex].answer_1}</li>
+                <li class="list-group-item quiz-answer-card" id="quiz-content-answer-2" onclick="selectAnswer(${index},${questionIndex},2)">${questions[index][questionIndex].answer_2}</li>
+                <li class="list-group-item quiz-answer-card" id="quiz-content-answer-3" onclick="selectAnswer(${index},${questionIndex},3)">${questions[index][questionIndex].answer_3}</li>
+                <li class="list-group-item quiz-answer-card" id="quiz-content-answer-4" onclick="selectAnswer(${index},${questionIndex},4)">${questions[index][questionIndex].answer_4}</li>
             </ol>
             </div>
             <nav aria-label="Page navigation example">
             <ul class="pagination centralized">
                 <li class="page-item">
-                <a class="page-link" href="#" aria-label="Previous">
+                <button class="page-link quiz-answer-card" onclick="previousQuestion(${index}, ${questionIndex}, ${topic})" aria-label="Previous">
                     <span aria-hidden="true">&laquo;</span>
-                </a>
+                </button>
                 </li>
-                <b id="current-question-index">${currentQuestionIndex}</b> / <b id="max-question-index">${questions[index].length}</b>
+                <b id="current-question-index">${currentQuestionNumber}</b> / <b id="max-question-index">${questions[index].length}</b>
                 <li class="page-item">
-                <a class="page-link" href="#" aria-label="Next">
+                <button id="next-question-button" class="page-link quiz-answer-card" onclick="nextQuestion(${index}, ${questionIndex}, ${topic})" aria-label="Next" disabled>
                     <span aria-hidden="true">&raquo;</span>
-                </a>
+                </button>
                 </li>
             </ul>
             </nav>
